094cc93ddb6f3f30f6d148adb35e8672100f3dcb
Даниил Шкельдин <denloader@mail.ru>
2022-12-15 15:08:23 +0300

    test2

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def special(char_name, char_class):
     if char_class == 'warrior': 
         return (f'{char_name} применил специальное умение «Выносливость {80 + 25}»')
     if char_class == 'mage':
-        return (f'{char_name} применил специальное умение «Атака {5 + 40}»')
+        return (f'{char_name} применил специальное умение «Атака {5 + 4-0}»')
     if char_class == 'healer':
         return (f'{char_name} применил специальное умение «Защита {10 + 30}»')
 
